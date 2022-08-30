8256475e42d4fd83da499e69ca2ea088ff84c081
FluffyBullet <103460601+FluffyBullet@users.noreply.github.com>
2022-08-30 22:12:06 +0100

    creating opening function and placements

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 print("Welcome to my Sodoku Challenge application!\n")
-print("Before we start, let us know your name:")
-user = input("Please enter your name ")
 
-print(f"Welcome {user}, lets get started!\n Now select your difficulty: 1. Easy, 2. Medium, 3. Hard")
-difficulty = input("type selection number here: \n")
+def intro():
+    print("Before we start, let us know your name:")
+    user = input("Please enter your name ")
 
-print(f"Thank you {user}, you have selected {difficulty}")
+    print(f"Welcome {user}, lets get started!\n Please select your difficulty: 1. Easy, 2. Medium, 3. Hard")
+    setting = input("type selection number here: \n")
+
+def run():
+    intro()
+run()
