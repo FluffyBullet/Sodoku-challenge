fe9bab328523b5ac2328fd9f2cff7896e3063e54
FluffyBullet <103460601+FluffyBullet@users.noreply.github.com>
2022-08-31 23:51:20 +0100

    addition of docstrings in functions

--- run.py
+++ run.py
@@ -1,6 +1,30 @@
-print("Welcome to my Sodoku Challenge application!\n")
+def get_difficulty():
+    """
+    Allows the user to select the difficulty of the game they are starting.
+    """
+    print("Enter your difficulty setting, \n 1 for Easy \n 2 for Medium \n 3 for Hard \n")
+    while True:
+        setting = input()
+        difficulty = ""
+        try:
+            if int(setting) == 1:
+                difficulty = "easy"
+            elif int(setting) == 2:
+                difficulty = "medium"
+            elif int(setting) == 3:
+                difficulty = "hard"
+            elif int(setting) != 1 or 2 or 3:
+                raise AttributeError
+            return difficulty
+        except AttributeError as e:
+            print(f"{e} invalid reference, please enter play/rules/exit")
+            return False
+
 
 def start_selection(mode):
+    """
+    request for user to select either rules to play the game, or to star the the game.
+    """
     try:
         if mode.lower() == "play":
             return True
@@ -35,4 +59,5 @@ def intro():
 
 def run():
     intro()
+    get_difficulty()
 run()
