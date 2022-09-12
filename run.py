6f0739ffca82041400956f2761ff96cd66ac428b
FluffyBullet <davidreynolds34@hotmail.co.uk>
2022-09-12 10:22:15 +0000

    removal of list_update func. not working

--- run.py
+++ run.py
@@ -71,67 +71,69 @@ def play_game(pull_puzzle, pull_answer):
     Function to hold the body of Sudoku puzzle
     """
 
-    # Color formatting for text
+# Color formatting for text
     _p_y = Fore.YELLOW
     _p_g = Fore.GREEN
     _p_r = Fore.RED
     _p_reset = Style.RESET_ALL
 
-    # Response to user to show still working
+# Response to user to show still working
     print("Your puzzle is as follows:")
-
+# Pulling data from text files
     with open(pull_answer + ".txt") as a:
         answer = a.readlines()
     with open(pull_puzzle + ".txt") as f:
         puzzle = f.readlines()
 
-    # Converting display & puzzle to number array
+# Converting display & puzzle to number array
     for i in range(len(answer)):
         answer[i] = answer[i].strip('\n').split(',')
     for i in range(len(puzzle)):
         puzzle[i] = puzzle[i].strip('\n').split(',')
 
-    # Formatting  for table to display the quiz.
+# Formatting  for table to display the quiz.
     t.field_names = ["XY", "A", "B", "C", "D", "E", "F", "G", "H", "I"]
     t._min_width = {"A": 4, "C": 4, "F": 4}
     t.align["A"] = "r"
     t.align["C"] = "l"
     t.align["F"] = "l"
 
+# Populating PrettyTable with information
     for line in range(len(puzzle)):
         t.add_rows([puzzle[line]])
 
+# variable for while condition - if game is complete
     _puzzle = []
     for item in puzzle:
         for _item in item:
             _puzzle.append(_item)
 
+# Creating variables for entry validation.
     possible_answers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
     grid_locations = []
-
     # search for all grid reference options (saves typing)
     for x in range(97, 106):
         for y in range(1, 10):
             locations = chr(x) + str(y)
             grid_locations.append(locations)
-    
-    # Variables for score/performance
+
+# Variables for score/performance
     guesses = 0
     s_time = time.time()
     # start time in seconds
     print(f"Time started = {s_time}")
 
     while "0" in _puzzle:
-        # Displays puzzle to the user
+# Displays puzzle to the user
         print(t)
-        # Requesting user to enter field and guess
+# Requesting user to enter field and guess
         grid_entry = input(_p_y + "Your grid ref: \n" + _p_reset)
         answer_entry = input(_p_y + "your guess: \n" + _p_reset)
 
         print(f"Your entry is {answer_entry} in {grid_entry}")
         print("Checking if your answer is correct....")
 
-        # Check if entry matches answer            
+# Check if entry matches answer         
         if validate_entry(possible_answers, answer_entry) is True:
             if validate_entry(grid_locations, grid_entry.lower()) is True:
                 if test_entry(answer, grid_entry, answer_entry, puzzle) is True:
@@ -146,18 +148,6 @@ def play_game(pull_puzzle, pull_answer):
                     print(f"Total guesses = {guesses}")
 
     end_game(guesses, s_time)
-
-
-def list_display(display, location, answer):
-    """
-    Updates _puzzle for while statement in play_game()
-    """
-    _puzzle = []
-    for item in display:
-        for _item in item:
-            _puzzle.append(_item)
-    return _puzzle
-
 
 def validate_entry(official, entry):
     """
@@ -179,12 +169,12 @@ def test_entry(answer, grid_entry, answer_entry, puzzle,):
     checks if guess v answer is correct.
     """
     _p_reset = Style.RESET_ALL
-    # To split guess location into single location identifiers
+# To split guess location into single location identifiers
     grid = grid_entry
     grid_x = grid[0].lower()
     grid_y = grid[1]
 
-    # Converts grid a-j ref to numeric value
+# Converts grid a-j ref to numeric value
     if grid_x == 'a':
         grid_x = 0
     elif grid_x == 'b':
@@ -207,30 +197,36 @@ def test_entry(answer, grid_entry, answer_entry, puzzle,):
         grid_x = 9
 
     if puzzle[int(grid_y)-1][int(grid_x)+1] != "0":
+
+# Checks if entry field has already been entered
         print(Fore.RED + "This field has already been entered" + _p_reset)
         return False
     elif int(answer[int(grid_y)-1][int(grid_x)]) == int(answer_entry):
-        # formats answer to display in color
+# formats answer to display in color
         answer_entry = Fore.GREEN + answer_entry + Style.RESET_ALL
-        #Updates puzzle display with new guess
+# Updates puzzle display with new guess
         puzzle[int(grid_y)-1][int(grid_x)+1] = answer_entry
-        location = puzzle[int(grid_y)-1][int(grid_x)+1]
-        list_display(puzzle, location, answer_entry)
-        # re-populates PrettyTable
+        location = [int(grid_y)-1] + [int(grid_x)+1]
+        print(location)
+# Re-populates PrettyTable
         t.clear_rows()
         for line in range(len(puzzle)):
-            t.add_rows([puzzle[line]]) 
-        
+            t.add_rows([puzzle[line]])
 
-    # if statement does not update _puzzle, does not update above
+# If statement does not update _puzzle, does not update above
         return True
     else:
         return False
 
 
 def end_game(guesses, s_time):
+    """
+    On completion summarize details of users performance.
+    """
+# Calculates total time in seconds spent
     f_time = time.time()
     t_time = int(s_time) - int(f_time)
+# Displays users performance
     print("Congratulations for completing the game!")
     print(f"Total guesses to completion = {guesses}")
     print(f"Total time to complete = {t_time}")
@@ -266,5 +262,4 @@ run()
 
 # implement timestamp at starting of game
 # implement timestamp at end of game
-# including attempted guesses
 # leaderboard to show time and guesses, selectable boards ?
